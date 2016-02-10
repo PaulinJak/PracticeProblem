9363380ca25aaee2662818c9939b50810526a69e
Jean Perier <jean.perier.polytechnique@gmail.com>
2016-02-10 23:03:19 +0100

    fonctions carre

---
+++ src/carre.hpp
@@ -0,0 +1,8 @@
+#ifndef __CARRE_HPP__
+#define __CARRE_HPP__
+#include "matrix.hpp"
+
+void fcarre(const Matrix2D<int> &image, const Matrix2D<int> &carre, const Matrix2D<int> &carre_score,const int i, const int j );
+
+
+#endif  /*__CARRE_HPP__*/
